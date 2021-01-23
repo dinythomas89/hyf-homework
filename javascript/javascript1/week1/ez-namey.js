432e776424f5978c342dbf196ecb7ba44662866d
dinythomas89 <dinythomas89@gmail.com>
2021-01-23 12:58:25 +0100

    changed , to + and modified the expression

--- javascript/javascript1/week1/ez-namey.js
+++ javascript/javascript1/week1/ez-namey.js
@@ -1,5 +1,5 @@
 const firstWords = ['Word', 'Fresh', 'Brand', 'Name', 'Quick', 'Innovative', 'Domain', 'Brand', 'One', 'Biz'];
 const secondWords = ['Lab', 'Books', 'Root', 'Station', 'Solutions', 'Creations', 'Puzzler', 'Bucket', 'Click', 'Namez'];
-const randomNumber = Math.floor(Math.random() * 10) + 0;
+let randomNumber = Math.floor(Math.random() * firstWords.length);
 let startupName = firstWords[randomNumber] + secondWords[randomNumber];
-console.log('The startup:',startupName,'contains',startupName.length,'characters');
+console.log('The startup: '+startupName+' contains '+startupName.length+' characters');
