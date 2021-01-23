380b30f8732b91989a676ad4d4eca9a06be396f0
dinythomas89 <dinythomas89@gmail.com>
2021-01-23 13:00:10 +0100

    removed the unnecessary comparison op

--- javascript/javascript1/week1/housey-pricey.js
+++ javascript/javascript1/week1/housey-pricey.js
@@ -5,5 +5,5 @@ const juliaGardenSize = 70;
 const peterHousePrice = peterHousevolume * 2.5 * 1000 + peterGardenSize * 300;
 const juliaHousePrice = juliaHouseVolume * 2.5 * 1000 + juliaGardenSize * 300;
 
-console.log((peterHousePrice < 2500000) === true ? 'Peter is paying more' : 'peter is paying less');
-console.log((juliaHousePrice < 1000000) === true ? 'Julia is paying more' : 'Julia is paying less');
+console.log((peterHousePrice < 2500000) ? 'Peter is paying more' : 'peter is paying less');
+console.log((juliaHousePrice < 1000000) ? 'Julia is paying more' : 'Julia is paying less');
