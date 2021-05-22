56f7665d1cc96b25771703d02655f4cf93332764
diny <dinythomas89@gmail.com>
2021-05-22 13:26:10 +0200

    implement the feedback as per mentors suggestion

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -1,6 +1,9 @@
 const { request } = require("express");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
