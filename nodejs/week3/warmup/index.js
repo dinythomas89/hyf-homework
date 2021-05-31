abfc00719d12f299ef5217c4532f63ecf53a5f99
diny <dinythomas89@gmail.com>
2021-05-31 12:08:54 +0200

    Revert "implement the feedback as per mentors suggestion"
    This reverts commit 56f7665d1cc96b25771703d02655f4cf93332764.

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -1,9 +1,6 @@
 const { request } = require("express");
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
